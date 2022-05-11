7a25a94e300bfeacb30f4c853ec8ead6193921c9
staskosh <staskoshenkov@rambler.ru>
2022-05-11 12:28:26 +0300

    Add a tg echo bot.

--- vk_quiz_bot.py
+++ vk_quiz_bot.py
@@ -1,6 +1,26 @@
 import os
 
 from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+tg_logger = logging.getLogger(__name__)
+
+
+def start(bot, update):
+    update.message.reply_text(f'Hi {update.message.chat.username}!')
+
+
+def echo(bot, update):
+    update.message.reply_text(update.message.text)
+
+
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
 
 
 def get_questions_and_answers(book_directory):
@@ -20,6 +40,20 @@ def main() -> None:
     load_dotenv()
     book_directory = os.getenv('QUIZ_QUESTIONS_FOLDER')
     questions_and_answers = get_questions_and_answers(book_directory)
+    updater = Updater(os.getenv("TG_TOKEN"))
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+
+    updater.idle()
 
 
 if __name__ == '__main__':
