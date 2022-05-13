69bbc8b47d3bc3929abf78b4c77a699cadc5b8f1
staskosh <staskoshenkov@rambler.ru>
2022-05-13 14:47:37 +0300

    Check right and wrong answers

--- vk_quiz_bot.py
+++ vk_quiz_bot.py
@@ -1,5 +1,7 @@
 import os
 import random
+from codecs import decode
+from functools import partial
 
 import redis
 import telegram
@@ -28,35 +30,23 @@ def get_questions_and_answers(book_directory):
         for index, item in enumerate(split_quiz):
             question_number, *question = item.split(':')
             if question_number.startswith('Вопрос'):
-                questions_and_answers[item] = split_quiz[index + 1]
+                _, split_answer = split_quiz[index + 1].split('Ответ:\n')
+                questions_and_answers[item] = split_answer
         return questions_and_answers
 
 
 def start(bot, update):
-    update.message.reply_text(f'Hi {update.message.chat.username}!')
+    update.message.reply_text(f'Привет, {update.message.chat.username} \n Я бот для викторин!')
 
 
-def send_question(bot, update):
-    text = "Привет, я бот для викторин"
-    redis_db = redis.Redis(
-        host='redis-15822.c300.eu-central-1-1.ec2.cloud.redislabs.com',
-        port=os.getenv('REDIS_PORT'),
-        db=0,
-        password='sC8nM6txxSrE9nZuosfQp0PpWv9n3bOD',
-    )
+def send_question(bot, update, redis_db=None, questions_and_answers=None):
     keyboard = [
         ['Новый вопрос', 'Сдаться'],
         ['Мой счет', ]
     ]
-    update.message.reply_text(
-        text,
-        reply_markup=telegram.ReplyKeyboardMarkup(
-            keyboard,
-            resize_keyboard=True,
-        )
-    )
+
     if update.message.text == 'Новый вопрос':
-        questions_and_answers = get_questions_and_answers(os.getenv('QUIZ_QUESTIONS_FOLDER'))
+
         random_question = random.choice(list(questions_and_answers))
         bot.send_message(
             chat_id=update.message.chat.id,
@@ -66,8 +56,38 @@ def send_question(bot, update):
                 resize_keyboard=True,
             )
         )
-        redis_db.set(update.message.chat.id, random_question)
-        redis_db.get(update.message.chat.id)
+        redis_db.set(update.message.chat.id, questions_and_answers[random_question])
+        answer = decode(redis_db.get(update.message.chat.id))
+        without_point_answer, *_ = answer.split('.')
+        without_parenthesis_answer, *_ = answer.split('(')
+        print(without_point_answer)
+        return update.message.text is False
+
+    answer = decode(redis_db.get(update.message.chat.id))
+    without_point_answer, *_ = answer.split('.')
+    without_parenthesis_answer, *_ = answer.split('(')
+    print(without_point_answer)
+
+    if update.message.text.startswith(without_point_answer) or update.message.text.startswith(without_parenthesis_answer):
+        bot.send_message(
+            chat_id=update.message.chat.id,
+            text='Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»',
+            reply_markup=telegram.ReplyKeyboardMarkup(
+                keyboard,
+                resize_keyboard=True,
+            )
+        )
+        return update.message.text is True
+
+    if update.message.text is not True:
+        bot.send_message(
+            chat_id=update.message.chat.id,
+            text='Неправильно… Попробуешь ещё раз?',
+            reply_markup=telegram.ReplyKeyboardMarkup(
+                keyboard,
+                resize_keyboard=True,
+            )
+        )
 
 
 def error(bot, update, error):
@@ -76,7 +96,13 @@ def error(bot, update, error):
 
 def main() -> None:
     load_dotenv()
-
+    redis_db = redis.Redis(
+        host=os.getenv('REDIS_HOST'),
+        port=os.getenv('REDIS_PORT'),
+        db=0,
+        password=os.getenv('REDIS_PASSWORD'),
+    )
+    questions_and_answers = get_questions_and_answers(os.getenv('QUIZ_QUESTIONS_FOLDER'))
     updater = Updater(os.getenv('TG_TOKEN'))
 
     dp = updater.dispatcher
@@ -84,7 +110,16 @@ def main() -> None:
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('help', help))
 
-    dp.add_handler(MessageHandler(Filters.text, send_question))
+    dp.add_handler(
+        MessageHandler(
+            Filters.text,
+            partial(
+                send_question,
+                redis_db=redis_db,
+                questions_and_answers=questions_and_answers
+            )
+        )
+    )
 
     dp.add_error_handler(error)
 
