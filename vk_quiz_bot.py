cd844597f5333dceeb1ba0d46f5806caf8be6ea2
staskosh <staskoshenkov@rambler.ru>
2022-05-18 10:55:12 +0300

    Add runtime.txt for deploy, add a vk username for the first message.

--- vk_quiz_bot.py
+++ vk_quiz_bot.py
@@ -8,14 +8,68 @@ from dotenv import load_dotenv
 from vk_api.keyboard import VkKeyboard
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-# def get_response(event, vk_api):
-#     vk_api.messages.send(
-#         user_id=event.user_id,
-#         message=event.text,
-#         keyboard=keyboard.get_keyboard(),
-#         random_id=random.randint(1,1000)
-#     )
 from get_answers_and_questions import get_questions_and_answers
+
+
+def get_answer(vk_api, longpoll, redis_db, keyboard):
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            answer = decode(redis_db.get(event.user_id))
+            without_point_answer, *_ = answer.split('.')
+            without_parenthesis_answer, *_ = answer.split('(')
+
+            if event.text.startswith(without_point_answer) or event.text.startswith(
+                    without_parenthesis_answer):
+                vk_api.messages.send(
+                    user_id=event.user_id,
+                    message='Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»',
+                    keyboard=keyboard.get_keyboard(),
+                    random_id=random.randint(1, 1000)
+                )
+                break
+
+            if event.text == "Сдаться":
+                vk_api.messages.send(
+                    user_id=event.user_id,
+                    message=f'Правильный ответ {answer}. Для следующего вопроса нажми «Новый вопрос»',
+                    keyboard=keyboard.get_keyboard(),
+                    random_id=random.randint(1, 1000)
+                )
+                break
+
+            if event.text:
+                vk_api.messages.send(
+                    user_id=event.user_id,
+                    message='Неправильно… Попробуешь ещё раз?',
+                    keyboard=keyboard.get_keyboard(),
+                    random_id=random.randint(1, 1000)
+                )
+
+
+def get_response(vk_api, longpoll, redis_db, questions_and_answers, keyboard):
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            if not event.text == "Новый вопрос" or event.text == "Сдаться" or event.text == "Мой счет":
+                user, *_ = vk_api.users.get(user_ids=event.user_id)
+                first_name = user['first_name']
+                vk_api.messages.send(
+                    user_id=event.user_id,
+                    message=f'Привет, {first_name} \n Я бот для викторин!',
+                    keyboard=keyboard.get_keyboard(),
+                    random_id=random.randint(1, 1000)
+                )
+
+            if event.text == "Новый вопрос":
+                random_question = random.choice(list(questions_and_answers))
+                vk_api.messages.send(
+                    user_id=event.user_id,
+                    message=random_question,
+                    keyboard=keyboard.get_keyboard(),
+                    random_id=random.randint(1, 1000)
+                )
+
+                redis_db.set(event.user_id, questions_and_answers[random_question])
+                get_answer(vk_api, longpoll, redis_db, keyboard)
 
 
 def main() -> None:
@@ -39,59 +93,9 @@ def main() -> None:
 
     keyboard.add_button('Мой счет')
 
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            vk_api.messages.send(
-                user_id=event.user_id,
-                message=f'Привет, {event.user_id} \n Я бот для викторин!',
-                keyboard=keyboard.get_keyboard(),
-                random_id=random.randint(1, 1000)
-            )
+    get_response(vk_api, longpoll, redis_db, questions_and_answers, keyboard)
 
-            if event.text == "Новый вопрос":
-                random_question = random.choice(list(questions_and_answers))
-                vk_api.messages.send(
-                    user_id=event.user_id,
-                    message=random_question,
-                    keyboard=keyboard.get_keyboard(),
-                    random_id=random.randint(1, 1000)
-                )
 
-                redis_db.set(event.user_id, questions_and_answers[random_question])
-
-                for event in longpoll.listen():
-                    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-                        answer = decode(redis_db.get(event.user_id))
-                        without_point_answer, *_ = answer.split('.')
-                        without_parenthesis_answer, *_ = answer.split('(')
-                        print(answer)
-
-                        if event.text.startswith(without_point_answer) or event.text.startswith(
-                                without_parenthesis_answer):
-                            vk_api.messages.send(
-                                user_id=event.user_id,
-                                message='Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»',
-                                keyboard=keyboard.get_keyboard(),
-                                random_id=random.randint(1, 1000)
-                            )
-                            break
-
-                        if event.text == "Сдаться":
-                            vk_api.messages.send(
-                                user_id=event.user_id,
-                                message=f'Правильный ответ {answer}. Для следующего вопроса нажми «Новый вопрос»',
-                                keyboard=keyboard.get_keyboard(),
-                                random_id=random.randint(1, 1000)
-                            )
-                            break
-
-                        if event.text:
-                            vk_api.messages.send(
-                                user_id=event.user_id,
-                                message='Неправильно… Попробуешь ещё раз?',
-                                keyboard=keyboard.get_keyboard(),
-                                random_id=random.randint(1, 1000)
-                            )
 
 
 if __name__ == "__main__":
